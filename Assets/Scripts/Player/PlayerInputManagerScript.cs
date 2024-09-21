2356167397b7afa73237375b35c417fdd99184cd
ItsWybro1 <54087253+ItsWybro1@users.noreply.github.com>
2024-09-20 22:47:09 -0600

    player input and movment
    * set up player input
    * set up player movement

--- Assets/Scripts/Player/PlayerInputManagerScript.cs
+++ Assets/Scripts/Player/PlayerInputManagerScript.cs
@@ -1,18 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInputManagerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Gamepad gamepad;
+    private PlayerMovement move;
+   // private PlayerMovement move;
+
+    [HideInInspector] public bool is_on;
+
+    private void Awake()
+    {
+        Initialize();
+        Activate();
+    }
+
+    public void Initialize()
+    {
+        /*if (GetComponentInChildren<PlayerInput>().GetDevice<Gamepad>())
+            gamepad = GetComponentInChildren<PlayerInput>().GetDevice<Gamepad>();*/
+        move = GetComponentInChildren<PlayerMovement>();
+
+        move.Initialize();
+
+        //add gp disconecct
+    }
+
+    public void OnMove(InputValue val)
+    { 
+        move.UpdateDirection(val.Get<Vector2>());
+        //move.UpdateDirection((Vector2)val.Get<Vector3>());
+        print("onmove");
+    }
+
+    public IEnumerator CoMove()
+    {
+        while (true)
+        {
+            if(gamepad != null)
+                move.UpdateDirection(gamepad.leftStick.ReadValue());
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public void OnThrow(InputValue val)
+    {
+
+    }
+
+    public void OnPush(InputValue val) 
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Activate()
     {
-        
+        is_on = true;
+        StartCoroutine(nameof(CoMove));
+    }
+
+    public void Dectivate()
+    {
+        is_on = true;
+        StopCoroutine(nameof(CoMove));
+        move.UpdateDirection(Vector2.zero);
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public float max_speed, acceleration, deacceleration;
+
+    private Vector2 direction;
+    private Rigidbody2D rb;
+    private float cur_speed;
+    private bool is_on, is_stun;
+
+    public void Initialize()
+    { 
+        rb = GetComponent<Rigidbody2D>();
+
+        //debug
+        Activate();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateDirection(Vector2 dir)
+    { 
+        direction = dir;
+        direction.Normalize();
+        print("update dir to " + dir);
+    }
+
+    public void FixedUpdate()
     {
-        
+        if (!is_on || is_stun)
+            return;
+        Vector2 step = direction * max_speed * Time.fixedDeltaTime;
+        //step.Normalize();
+        //cur_speed += acceleration;
+        //step = step * cur_speed * Time.fixedDeltaTime;
+        step += (Vector2)transform.position;
+        print("step: " + step);
+        rb.MovePosition(step);
+    }
+
+    public void Activate()
+    {
+        is_on = true;
+    }
+
+    public void Deactivate()
+    {
+        is_on = false;
     }
 }
