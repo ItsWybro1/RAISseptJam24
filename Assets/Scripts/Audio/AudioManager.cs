1b6c4e3d6752a013c6a4382e9fd8ae71b3b977b2
PhotonLotus <godinaustin@gmail.com>
2024-09-21 18:00:15 -0600

    Audio stuff

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Sources")]
+    [SerializeField] AudioSource SFX;
+    [SerializeField] AudioSource Music;
+    [Header("Audio Clips")]
+    [SerializeField] AudioClip[] MenuSFX; // buttons and such 
+    [SerializeField] AudioClip[] GameSFX; // ball hit sounds, and wooshes, see #1 comment below
+    [SerializeField] AudioClip MenuMusic;
+    [SerializeField] AudioClip GameMusic;
+    //Instances the Audiomanager
+    private static AudioManager _instance;
+
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<AudioManager>();
+
+                //Tell unity not to destroy this object when loading a new scene!
+                DontDestroyOnLoad(_instance.gameObject);
+            }
+
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            if (this != _instance)
+                Destroy(this.gameObject);
+        }
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        if (Music.clip != clip)
+        {
+            Music.clip = clip;
+        }
+    }
+    public void PlaySFXoneshot(AudioClip[] clip, int i)
+    {
+        SFX.PlayOneShot(clip[i]);
+    }
+
+}
