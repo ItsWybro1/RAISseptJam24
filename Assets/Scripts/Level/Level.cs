1a77d0bfacf59f37924f59bdc8e0f7ad6124b0bd
Hero <321hero.mcpe@gmail.com>
2024-09-22 14:14:10 -0700

    Level Random Spawns

--- Assets/Scripts/Level/Level.cs
+++ Assets/Scripts/Level/Level.cs
@@ -7,6 +7,7 @@ public class Level : MonoBehaviour
     [SerializeField] private SpriteRenderer levelSprite;
     [SerializeField] private Transform ballSpawn1;
     [SerializeField] private Transform ballSpawn2;
+    [SerializeField] private Transform hazardSpawn1, hazardSpawn2;
 
     public List<Sprite> levelEnvironment = new List<Sprite>();
     public List<GameObject> hazards = new List<GameObject>();
@@ -15,16 +16,29 @@ public class Level : MonoBehaviour
 
     private void Start()
     {
-        Initialize();
+
     }
 
     public void Initialize()
     {
-        print(levelEnvironment.Count);
+        //LEVEL CHANGE
         levelSprite.sprite = levelEnvironment[Random.Range(0, levelEnvironment.Count)];
 
-        Instantiate(projectiles[Random.Range(0, projectiles.Count)], ballSpawn1);
-        Instantiate(projectiles[Random.Range(0, projectiles.Count)], ballSpawn2);
+        //SPAWN BALLS
+        GameObject ball1 = Instantiate(projectiles[Random.Range(0, projectiles.Count)], ballSpawn1);
+        GameObject ball2 = Instantiate(projectiles[Random.Range(0, projectiles.Count)], ballSpawn2);
+
+        //SPAWN HAZARDS
+        hazardSpawn1.position = new Vector2(Random.Range(-8, 8), Random.Range(-4, 4));
+        hazardSpawn2.position = new Vector2(Random.Range(-8, 8), Random.Range(-4, 4));
+
+        GameObject hazard1 = Instantiate(hazards[Random.Range(0, hazards.Count)], hazardSpawn1);
+        GameObject hazard2 = Instantiate(hazards[Random.Range(0, hazards.Count)], hazardSpawn2);
+    }
+
+    public void ResetLevel()
+    {
+        
     }
 
     // Update is called once per frame
