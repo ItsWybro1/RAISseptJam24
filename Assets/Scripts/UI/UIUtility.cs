f9ea9fb244aadd74343fdf6e1775b4e867be84dc
Insomniacwolf <157617851+Insomniacwolf@users.noreply.github.com>
2024-09-21 19:44:34 -0600

    made specific pause menu with toggle function

--- Assets/Scripts/UI/UIUtility.cs
+++ Assets/Scripts/UI/UIUtility.cs
@@ -6,47 +6,18 @@ using UnityEngine.SceneManagement;
 public class UIUtility : MonoBehaviour
 {
     public GameObject uiCanvas;
-    public GameObject pauseMenu;
-    private bool isPaused;
     public void Initialize()
     { 
-        uiCanvas.SetActive(true);
-        pauseMenu.SetActive(false);
+        uiCanvas.SetActive(true); 
         Time.timeScale = 1.0f;
-        isPaused = false;
     }
     public void Update()
     {
-        if(Input.GetKeyUp(KeyCode.P) && isPaused == false)
-        {
-            OnPausePress();
-        }
-        else if (Input.GetKeyUp(KeyCode.P) && isPaused == true)
-        {
-            OnResumePress();
-        }
 
     }
     public void LoadScene(string scene)
     {
         SceneManager.LoadScene(scene);
-    }
-    public void QuitToMenu()
-    {
-        SceneManager.LoadScene(0);
-    }
-
-    public void OnPausePress()
-    {
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0f;
-        isPaused = true;
-    }
-    public void OnResumePress()
-    {
-        pauseMenu.SetActive(false);
-        Time.timeScale = 1f;
-        isPaused = false;
     }
     public void QuitOutTheGame()
     {
